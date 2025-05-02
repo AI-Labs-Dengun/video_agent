298db3900e11ee1453a951cd0fd7f0712a852b33
leandro <leandro.justino@dengun.com>
2025-05-02 14:38:03 +0100

    vercel

---
+++ src/lib/language.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React, { createContext, useContext, useState, useEffect } from 'react';
+
+interface LanguageContextType {
+  language: string;
+  setLanguage: (lang: string) => void;
+}
+
+const LanguageContext = createContext<LanguageContextType>({
+  language: 'pt',
+  setLanguage: () => {},
+});
+
+export const LanguageProvider = ({ children }: { children: React.ReactNode }) => {
+  const [language, setLanguage] = useState('pt');
+
+  useEffect(() => {
+    const savedLanguage = localStorage.getItem('language');
+    if (savedLanguage) {
+      setLanguage(savedLanguage);
+    }
+  }, []);
+
+  const handleSetLanguage = (lang: string) => {
+    setLanguage(lang);
+    localStorage.setItem('language', lang);
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage: handleSetLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => useContext(LanguageContext); 
